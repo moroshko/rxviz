3ab0904f8895251cc773459466a94eda52374027
Misha Moroshko <michael.moroshko@gmail.com>
2017-06-08 16:15:24 -0700

    Extract SidebarLink component

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
-import Link from 'next/link';
 import codeExamples from '../lib/code-examples';
+import SidebarLink from './SidebarLink';
 
 const Sidebar = ({ activeItemId }) => {
   const exampleIds = Object.keys(codeExamples);
@@ -14,85 +14,50 @@ const Sidebar = ({ activeItemId }) => {
 
           return (
             <li className={`example ${exampleId}`} key={exampleId}>
-              <Link
-                prefetch
-                as={`/examples/${exampleId}`}
+              <SidebarLink
+                text={name}
+                isActive={exampleId === activeItemId}
                 href={`/?exampleId=${exampleId}`}
-              >
-                <a
-                  className={`link${exampleId === activeItemId
-                    ? ' active'
-                    : ''}`}
-                >
-                  {name}
-                </a>
-              </Link>
+                as={`/examples/${exampleId}`}
+              />
             </li>
           );
         })}
       </ul>
-      <Link prefetch href="/feedback">
-        <a
-          className={`feature link${activeItemId === 'feedback'
-            ? ' active'
-            : ''}`}
-        >
-          Feedback
-        </a>
-      </Link>
+      <SidebarLink
+        text="Feedback"
+        isActive={activeItemId === 'feedback'}
+        href="/feedback"
+      />
       <style jsx>{`
-          .container {
-            display: flex;
-            flex-direction: column;
-            flex-shrink: 0;
-          }
+        .container {
+          display: flex;
+          flex-direction: column;
+          flex-shrink: 0;
+        }
 
-          .header {
-            font-size: 18px;
-            padding-left: 25px;
-          }
+        .header {
+          font-size: 18px;
+          padding-left: 25px;
+        }
 
-          .examples-container {
-            display: flex;
-            flex-direction: column;
-            flex-grow: 1;
-            margin-top: 3px;
-            padding: 0;
-            overflow-y: auto;
-          }
+        .examples-container {
+          display: flex;
+          flex-direction: column;
+          flex-grow: 1;
+          margin-top: 3px;
+          padding: 0;
+          overflow-y: auto;
+        }
 
-          li {
-            padding: 0;
-          }
+        li {
+          padding: 0;
+        }
 
-          .custom {
-            margin-top: 10px;
-          }
-
-          .link {
-            padding: 12px 25px;
-            display: block;
-            color: #eeeff0;
-            text-decoration: none;
-            opacity: 0.6;
-            transition: opacity .1s linear;
-          }
-
-          .link:hover {
-            background-color: #2c3038;
-          }
-
-          .active {
-            padding-left: 22px;
-            border-left: 3px solid #e0168f;
-            background-color: #2c3038;
-            opacity: 1;
-          }
-
-          .feature {
-
-          }
-        `}</style>
+        .custom {
+          margin-top: 10px;
+        }
+      `}</style>
     </nav>
   );
 };
