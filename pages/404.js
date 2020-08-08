fe1182af0a8e6bbaaaf8d1553bc03db2430921b9
Raziel <3615018+rraziel@users.noreply.github.com>
2020-08-08 02:39:09 +0200

    Fixed missing return on the error404 component

--- pages/404.js
+++ pages/404.js
@@ -3,7 +3,7 @@ import Error from '../components/Error';
 const error404 = () => {
   const statusCode = 404;
 
-  <Error statusCode={statusCode} />;
+  return <Error statusCode={statusCode} />;
 };
 
 export default error404;
